21d2a1d7fa71939a2e3d715da94b70543f1bd2b7
mew-ton <mxl_usmarc@slmail.mewton.jp>
2023-12-19 12:34:13 +0900

    WIP

--- src/unreefy.ts
+++ src/unreefy.ts
@@ -1,6 +1,6 @@
-import type { Reefied, Unreefy } from ".";
+import type { Reefied, Unreefy } from '.'
 
 // biome-ignore lint/suspicious/noExplicitAny: <explanation>
-export  function unreefy<T extends Reefied<any, any>>(target: T): Unreefy<T> {
+export function unreefy<T extends Reefied<any, any>>(target: T): Unreefy<T> {
   return target
 }
